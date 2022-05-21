22a670d22de247ff95801f4f992e26d51fc71224
Chenbaoyu <1743678777@qq.com>
2022-05-21 14:07:01 +0800

    删除vue2项目，使用vueui创建vue3项目

---
+++ vue-blog/src/components/login/login.vue
@@ -0,0 +1,24 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: 'LoginPage',
+  props:{
+
+  },
+  data() {
+    return {}
+  },
+  methods() {
+
+  },
+  watch: {},
+  computed: {}
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue-blog/src/views/AboutView.vue
@@ -0,0 +1,23 @@
+<template>
+  <li v-for="n in evenNumbers" :key="n">{{ n }}</li>
+</template>
+<script>
+let Vue;
+Vue.createApp({
+  data() {
+    return {
+      numbers: [ 1, 2, 3, 4, 5 ]
+    }
+  },
+  computed: {
+    evenNumbers() {
+      return this.numbers.filter(number => number % 2 === 0)
+    }
+  }
+})
+
+
+</script>
+
+
+
