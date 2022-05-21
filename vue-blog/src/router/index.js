7f274ac5987803b4fc89d3274bbd8218f536fee9
Chenbaoyu <1743678777@qq.com>
2022-05-22 00:56:43 +0800

    由于vue3无法使用elementui，element plus使用ts文件，版本重新换为vue2”

--- vue-blog/src/router/index.js
+++ vue-blog/src/router/index.js
@@ -1,29 +1,21 @@
-import {createRouter, createWebHashHistory} from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
 
-const routes = [
+Vue.use(Router)
+
+export default new Router({
+  routes: [
     {
-        path: '/',
-        name: 'home',
-        component: HomeView
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
     },
     {
-        path: '/about',
-        name: 'about',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-    }, {
-        path: "/login",
-        name: "login",
-        component: () => import('../components/login/login')
+      path: '/login',
+      name: 'login',
+      component:()=>import('../view/login/login')
     }
-]
 
-const router = createRouter({
-    history: createWebHashHistory(),
-    routes
+  ]
 })
-
-export default router
